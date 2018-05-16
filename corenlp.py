cd4fda4147b764c4f29fdf5d9f6d0971760f1879
anantupadhyay <anantupadhyay17@gmail.com>
2018-05-16 18:18:01 +0530

    First Commit of file

---
+++ corenlp.py
@@ -0,0 +1,117 @@
+from pycorenlp import StanfordCoreNLP
+import json
+
+def getStanfordAnalysis(text):
+	try:
+		nlp = StanfordCoreNLP('http://13.127.253.52:9000/')
+		output = nlp.annotate(text, properties={'annotators': 'dcoref','outputFormat':'json'})#'annotators': 'tokenize,ssplit,pos,depparse,parse,dcoref'
+		#print(output['sentences'])
+		#print getSentences(output['sentences'])
+		#for i in range(0,len(output['sentences'])):
+		# 	output['sentences'][i]['sentence']=getSentences(output['sentences'][i]['tokens'])
+		# 	sentence_list.append(output['sentences'][i]['sentence'])
+
+		# lock.acquire()
+		# combine_dict['stanfordAnalysis'] =output
+		# print 'sucess stanford'#,output
+		# lock.release()
+		
+		wjson = json.dumps(output['sentences'])
+		wjdata = json.loads(wjson)
+		#print wjdata
+		key = set()
+		val = set()
+		nmod = []
+		case = dict()
+		for x in range(len(wjdata[0]['basicDependencies'])):
+
+			if (wjdata[0]['basicDependencies'][x]['dep'] == 'nummod'):
+				key.add(wjdata[0]['basicDependencies'][x]['governorGloss'])
+				val.add(wjdata[0]['basicDependencies'][x]['dependentGloss'])
+			
+			
+			if(wjdata[0]['basicDependencies'][x]['dep'] == 'advmod'):
+				if((wjdata[0]['basicDependencies'][x]['dependentGloss']) not in key):
+					val.add(wjdata[0]['basicDependencies'][x]['dependentGloss'])
+				
+				if((wjdata[0]['basicDependencies'][x]['governorGloss']) not in val):
+					key.add(wjdata[0]['basicDependencies'][x]['governorGloss'])
+			
+			if(wjdata[0]['basicDependencies'][x]['dep'] == 'amod'):
+				if((wjdata[0]['basicDependencies'][x]['dependentGloss']) not in key):
+					val.add(wjdata[0]['basicDependencies'][x]['dependentGloss'])
+				if((wjdata[0]['basicDependencies'][x]['governorGloss']) not in val):
+					key.add(wjdata[0]['basicDependencies'][x]['governorGloss'])
+			
+			if(wjdata[0]['basicDependencies'][x]['dep'] == 'nsubj' or wjdata[0]['basicDependencies'][x]['dep'] == 'nsubjpass'):
+				if((wjdata[0]['basicDependencies'][x]['dependentGloss']) not in val):
+					key.add(wjdata[0]['basicDependencies'][x]['dependentGloss'])
+				if((wjdata[0]['basicDependencies'][x]['governorGloss']) not in key):
+					val.add(wjdata[0]['basicDependencies'][x]['governorGloss'])
+			
+			if(wjdata[0]['basicDependencies'][x]['dep'] == 'nmod'):
+				if((wjdata[0]['basicDependencies'][x]['governorGloss']) not in val):
+					key.add(wjdata[0]['basicDependencies'][x]['governorGloss'])
+				if((wjdata[0]['basicDependencies'][x]['dependentGloss'] not in key)):
+					val.add(wjdata[0]['basicDependencies'][x]['dependentGloss'])
+					nmod.append(wjdata[0]['basicDependencies'][x]['dependent'])
+
+			if(wjdata[0]['basicDependencies'][x]['dep'] == 'conj'):
+				if((wjdata[0]['basicDependencies'][x]['governorGloss']) not in val):
+					key.add(wjdata[0]['basicDependencies'][x]['governorGloss'])
+					if((wjdata[0]['basicDependencies'][x]['dependentGloss']) not in val):
+						key.add(wjdata[0]['basicDependencies'][x]['dependentGloss'])
+				else:
+					if((wjdata[0]['basicDependencies'][x]['dependentGloss']) not in val):
+						val.add(wjdata[0]['basicDependencies'][x]['dependentGloss'])
+
+
+			if(wjdata[0]['basicDependencies'][x]['dep'] == 'compound'):
+				if(wjdata[0]['basicDependencies'][x]['governorGloss'] in key):
+					key.add(wjdata[0]['basicDependencies'][x]['dependentGloss'])
+				elif(wjdata[0]['basicDependencies'][x]['governorGloss'] in val):
+					val.add(wjdata[0]['basicDependencies'][x]['dependentGloss'])
+
+			if(wjdata[0]['basicDependencies'][x]['dep'] == 'dobj'):
+				if(wjdata[0]['basicDependencies'][x]['governorGloss'] in key):
+					key.add(wjdata[0]['basicDependencies'][x]['dependentGloss'])
+				elif(wjdata[0]['basicDependencies'][x]['governorGloss'] in val):
+					val.add(wjdata[0]['basicDependencies'][x]['dependentGloss'])
+			
+			if(wjdata[0]['basicDependencies'][x]['dep'] == 'case'):
+				case[wjdata[0]['basicDependencies'][x]['governorGloss']] = wjdata[0]['basicDependencies'][x]['governor']
+			
+		for x in range(len(wjdata[0]['tokens'])):
+			if(wjdata[0]['tokens'][x]['pos'] == 'VB'):
+				if(wjdata[0]['tokens'][x]['originalText']) not in val:
+					key.add(wjdata[0]['tokens'][x]['originalText'])
+
+			if(wjdata[0]['tokens'][x]['pos'] == 'JJ'):
+				if(wjdata[0]['tokens'][x]['originalText']) not in key:
+					val.add(wjdata[0]['tokens'][x]['originalText'])
+			'''
+			if(wjdata[0]['tokens'][x]['pos'] == 'NN' or wjdata[0]['tokens'][x]['pos'] == 'NNS' or wjdata[0]['tokens'][x]['pos'] == 'NNP'):
+				if(wjdata[0]['tokens'][x]['originalText']) not in val:
+					key.add(wjdata[0]['tokens'][x]['originalText'])
+			'''
+		print nmod
+		print case
+		
+		remove_word = set()
+		for x in val:
+			if (x in case.keys()) and (case[x] in nmod):
+				remove_word.add(x)
+
+		val = val.difference(remove_word)
+		print ("key are ", key)
+		print ("values are ", val)
+		
+	
+	except Exception as e:
+		#print e
+		return 'failure',"Failed to Analyse Data, 9000 down"
+
+text = "The fan above the bed was dirty"
+print (text)
+
+print getStanfordAnalysis(text.lower())
