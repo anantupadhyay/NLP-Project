3acdbd042daae853410741469fee46edd5a57869
anantupadhyay <anantupadhyay17@gmail.com>
2018-05-17 18:11:45 +0530

    Testing

--- clean.py
+++ clean.py
@@ -5,26 +5,29 @@ def replace_escape_characters(foo):
 	idx = 1
 	while(idx!=-1):
 		idx = foo.find('\n')
-		#print idx
+		#print foo[idx-1]
 		if((foo[idx-1]==' ' and foo[idx-2] == '.') or foo[idx-1]=='.'):
 			foo = foo.replace('\n', ' ')
-		elif(foo[idx-1]==' ' and foo[idx-2]!='.'):
+		if(foo[idx-1]==' ' and foo[idx-2]!='.'):
 			foo = foo.replace(' \n', '. ')
+		elif(foo[idx-1]!=' ' and foo[idx-1]!='.'):
+			foo = foo.replace('\n', '. ')
 	idx = 1
 	while(idx != -1):
 		#print idx
 		idx = foo.find('\t')
 		foo = foo.replace('\t', ' ')
 
-	foo = re.sub(r'\.+', ".", foo)
-	foo = re.sub(r'\. .', ". ", foo)
+	print (foo)
+
+	#foo = re.sub('\. .', ". ", foo)
 	foo = re.sub(' +',' ', foo)
 	foo = foo.replace(" .", ".")
 	foo = re.sub(r'\.+', ".", foo)						# replacing '...*' with '.'
 	foo = re.sub(r'\?+', "?", foo)						# replacing '???*' with '?'
 	foo = re.sub(r'\!+', "!", foo)						# replacing '!*' with '!'
 	#foo = re.sub(r'\ .', ".", foo)
-	print foo	
+	#print foo	
 	return foo
 
 # def cleaner_function(text):
@@ -64,24 +67,23 @@ def replace_escape_characters(foo):
 # 	print('-'*100)
 # 	return txt
 
-# def load_doc(filename):
-# 	# open the file as read only
-# 	file = open(filename, 'r')
-# 	# read all text
-# 	text = file.read()
-# 	# close the file
-# 	file.close()
-# 	return text
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
  
 # emojis = [':-J', '=^_^=', ':-o', '=-D', '>_<', '(*_*)', '>_<*', '=)', '=(', ':-p', ':p', '=D',
 # 				':-/', ':/', '>-:o', '>:o', 'B)', 'B-)', '8-|', '8|', 'O:)', 'O:-)', '<3', ':)', ':D',
 # 				':(', ':-)', ':-(', ':P']
 
-# load the document
-# filename = 'sample.txt'
-# text = load_doc(filename)
-# print text
+#load the document
+filename = 'sample.txt'
+text = load_doc(filename)
 # clean_text = cleaner_function(text)
 # print('_'*100)
-clean_text = replace_escape_characters("this is not \n my.\n \n\n\n\n work\t\ttada")
+clean_text = replace_escape_characters("this property is like a guest house not a townhouse no room services avaialble no proper breakfast provided cold tea. I was given two separate bed instead of a king size one. There was construction going on just above my room which made it impossible to stay in the room. here were no water bottles in the room. here was no ac remote and when one was given it was not working either. he toilet seat was dirty. he most pathetic experience ever. Horrible experience full night i sleep in without electricity no fan no ac working please resolve other wise i never stay in oyo and share in my page about oyo horrible hotels. i asked them for a double quilt as i had two single but the reception guy said we dont keep extra. in lunch ordered shahi thali with , green chicken, but gavwe paneer. idnt gave anything from that. Hot water was available. thisssss is testtt stringgggggg. I did not check in, n yet the booking says I did. Will just anyone go n have a simple hygiene n cleaning audit. ? From the entrance door- big red color - misaligned. ift floor matt has enough dirt with bad odour comes from the reception lobby till the room. hecked in 205, bedsheets were dirty with mark on the pillow covers. athrooms flush is falling apart, shower curtain are stained badly.then shifted to room number 305, same issue there also but somehow compromised as we were very tried. In room , they should provide glass, tea cup and electric kettle. they should also provide food menu card , room service number, operator numbers. the hotel was awsm :). this is no t \n my.\n \n\n\n\n work\t\ttada")
 print (clean_text)
