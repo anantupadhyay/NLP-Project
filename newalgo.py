eabdc3c7e9433ad5ae0c49249ba3bbd8b636ef4f
anantupadhyay <anantupadhyay17@gmail.com>
2018-05-22 17:55:19 +0530

    Major Changes Made

--- newalgo.py
+++ newalgo.py
@@ -1,5 +1,12 @@
 from pycorenlp import StanfordCoreNLP
+from nltk.tree import ParentedTree, Tree
+from nltk.parse.stanford import StanfordParser
+import difflib
 import json
+
+# Defining the global dictionaries
+rel = dict()
+rel2 = dict()
 
 def getStanfordAnalysis(text):
 	try:
@@ -21,15 +28,14 @@ def getStanfordAnalysis(text):
 			if((wjdata[0]['tokens'][x]['pos'] == 'NN') or (wjdata[0]['tokens'][x]['pos'] == 'NNP') or (wjdata[0]['tokens'][x]['pos'] == 'NNS') or (wjdata[0]['tokens'][x]['pos'] == 'PRP')):
 				noun[wjdata[0]['tokens'][x]['index']] = wjdata[0]['tokens'][x]['word']
 
-		print ("The extracted nouns from the sentence are", noun)
-		print ('#'*100)
+		# print ("The extracted nouns from the sentence are", noun)
+		# print ('#'*100)
 
 # Code for extracting noun ends here!		
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		
 # The Code here deals with extracting the dependencies of noun with adjectives and verbs
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		rel = dict()
 		#Buddy Check token also
 		for x in range(len(wjdata[0]['enhancedPlusPlusDependencies'])):
 			if((wjdata[0]['enhancedPlusPlusDependencies'][x]['dependent']) in noun.keys()):
@@ -128,10 +134,16 @@ def getStanfordAnalysis(text):
 # =========================================================================================================================================================================================================================================
 
 # This part prints the final key value pair
-
-		print ("Here are the final key value pairs\n")
+		
 		for key, val in rel.items():
-			print (key, val)
+			st = ""
+			for word in text.split():
+				#print word
+				if word in val:
+					st += word + " "
+			rel[key] = st
+
+		print rel
 
 # The part reports error, if any, occured in the code
 
@@ -143,10 +155,204 @@ def getStanfordAnalysis(text):
 #.........................................................................................................................................................
 #*********************************************************************************************************************************************************
 
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+'''
+	CODE THAT UTILIZES THE PARSE TREE OF THE SENTENCE STARTS HERE
+'''
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def find_attributes(node):
+	attrs = []
+	dad = node.parent()
+	gdad = dad.parent()
+	#print node, dad, gdad
+	#print node
+	# Searching all the siblings of the node
+	for sibling in dad:
+		if ((sibling.label() == 'JJ') or (sibling.label() == 'RB') or (sibling.label() == 'CD')):
+			attrs.append(sibling[0])
+		#print sibling[0][0]
+
+	# Searching all the uncles of the node
+	for uncle in gdad:
+		#print uncle
+		# Checking if directly a adverb is present, then append it to attribute list
+		if((uncle.label() == 'RB')):
+			attrs.append(uncle[0])
+		
+		# If it is a verb phrase, then check all the children of the VP
+		
+		elif (uncle.label() == 'VP'):
+			for cousin in uncle:
+				#print cousin.label()
+				if cousin.label() == 'ADJP':
+					for ch in cousin:
+						attrs.append(ch[0])
+						#print ch[0]
+
+				elif ((cousin.label() == 'VBN') or (cousin.label() == 'VBG') or (cousin.label() == 'RB') or (cousin.label() == 'VBP') or (cousin.label() == 'VBZ')):
+					attrs.append(cousin[0])
+				# the leaves() method returns a list of node
+				elif cousin.label() == 'VP':
+					for ch in cousin:
+						ls = ch.leaves()
+						#print ls
+						fin_val = ""
+						for word in ls:
+							fin_val += (' '.join(ls))
+						attrs.append(fin_val)
+				# if cousin is a noun phrase
+				elif (cousin.label() == 'NP'):
+					attrs.append(cousin[0][0])
+
+
+	# Searching all the sibling of grand-parent of the node
+	# Here we are looking for verb phrase and its children only
+	ggdad = gdad.parent()
+	if(ggdad.label() != 'ROOT'):
+		for s in ggdad:
+			#print s
+			if s.label() == 'VP':
+				for uch in s:
+					#print uch
+					'''
+						If the children are adjective or adverb
+					'''
+					if uch.label() == 'ADJP':
+						for ch in uch:
+							attrs.append(ch[0])
+
+					elif uch.label() == 'ADVP':
+						for ch in uch:
+							attrs.append(ch[0])
+
+					# Check if it is a verb participle, then append it
+					elif uch.label() == 'VBP' or uch.label()=='VBZ' or uch.label()=='VBN' or uch.label()=='VBG':
+						attrs.append(uch[0])
+
+					elif uch.label() == 'NP':
+						for ch in uch:
+							#print ch[0]
+							attrs.append(ch[0])
+
+
+
+	return attrs
+
+def parsetreeAnalysis(text):
+	try:
+		nlp = StanfordCoreNLP('http://13.127.253.52:9000/')
+ 		output = nlp.annotate(text, properties={'annotators': 'dcoref','outputFormat':'json'})
+ 		parse_tree = output['sentences'][0]['parse']
+		tree = ParentedTree.convert(Tree.fromstring(parse_tree))
+		#tree.pretty_print()
+
+		np = dict()
+		# FINDING THE NP AND ITS CORRESPONDING NOUN OR PRONOUN
+		for s in tree.subtrees(lambda tree: tree.label() == 'NP'):
+			'''
+				This part deals with the problem that the child of ROOT comes as NP,
+				and hence, each noun was counted twice.
+			'''
+			for n in s.subtrees(lambda n: n.label().startswith('NN')):
+				vis = np.get(n[0], 0)
+				if(vis == 1):
+					continue
+				np[n[0]] = 1
+				rel2.setdefault(n[0], [])
+
+				attr = find_attributes(n)
+				rel2[n[0]] = attr
+				#print attr
+				if len(attr)==0:
+					rel2.pop(n[0])
+
+		#print rel2
+		for key, val in rel2.items():
+			st = ' '.join(val)
+			rel2[key] = st
+
+		print rel2
+
+	# THIS PART CHECKS FOR ERRORS, IF ANY, AND REPORTS THE FAILURE	
+	except Exception as e:
+ 		print e
+		return 'failure',"An error has occured, Failed to Analyse Data, 9000 down"
+# ---------------------------------------------------------------------------------------------------
+'''
+	CODE FOR PARSE TREE ENDS HERE
+'''
+# ---------------------------------------------------------------------------------------------------
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+'''
+	THIS PART MERGES THE TWO DICTIONARIES INTO ONE
+'''
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+def merge (l, r):
+    m = difflib.SequenceMatcher(None, l, r)
+    for o, i1, i2, j1, j2 in m.get_opcodes():
+        if o == 'equal':
+            yield l[i1:i2]
+        elif o == 'delete':
+            yield l[i1:i2]
+        elif o == 'insert':
+            yield r[j1:j2]
+        elif o == 'replace':
+            yield l[i1:i2]
+            yield r[j1:j2]
+
+def merge_dictionaries(rel, rel2):
+	fin_rel = dict()
+	for key, val in rel.items():
+		if key in rel2.keys():
+			val2 = rel2[key]
+			merged = merge(val.lower().split(), val2.lower().split())
+			new_val = ' '.join(' '.join(x) for x in merged)
+			fin_rel[key] = new_val
+		else:
+			fin_rel = val
+
+	for key, val in rel2.items():
+		if key not in fin_rel.keys():
+			fin_rel[key] = val
+
+
+	return fin_rel
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+'''
+	CODE FOR MERGING ENDS HERE
+'''
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+#-----------------------------------------------
+'''
+	MAIN FUNCTION STARTS HERE
+'''
+#-----------------------------------------------
 if __name__=="__main__" :
-	text = "The restaurant had only Indian cuisine"
+	text = "The tea is served hot here"
 	print (text)
 
 	res = (getStanfordAnalysis(text))
 	if(res != None):
 		print res
+
+	res2 = (parsetreeAnalysis(text))
+	if(res2 != None):
+		print res2
+
+	kvp = merge_dictionaries(rel, rel2)
+	print "\nThe final key value pairs are"
+	print kvp
+
+# ------------------------------------------------------
+'''
+	CONGRATS! YOU HAVE ARRIVED AT THE END OF CODE...
+'''
+# ------------------------------------------------------
