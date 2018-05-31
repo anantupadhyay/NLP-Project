4f7effe85306f6971dbbb2c6bab9afd78710dadc
anantupadhyay <anantupadhyay17@gmail.com>
2018-05-31 14:18:03 +0530

    Testing Commits

--- spell check.py
+++ spell check.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# from gingerit.gingerit import GingerIt
+from gingerit.gingerit import GingerIt
 
-# text = "the got busi working .the got busi working. the got busi working. the got busi working . the got busi working .the got busi working. the got busi working. the got busi working ."
-# text = "Klüft skräms inför på fédéral électoral große"
+text = "the got busi working .the got busi working. the got busi working. the got busi working . the got busi working .the got busi working. the got busi working. the got busi working ."
+#text = "Klüft skräms inför på fédéral électoral große"
 
-# parser = GingerIt()
-# print parser.parse(text)['result']
+parser = GingerIt()
+print parser.parse(text)['result'], '\n'
 
-# print text
+#print text
 
 
 from googletrans import Translator
@@ -18,10 +18,3 @@ txt = "Klüft skräms inför på fédéral électoral große stüff"
 spell = translator.translate(txt, dest='en')
 op = (spell.text).encode("utf-8")
 print op
-
-# import coreference_resolution as cr 
-
-# text = "The got busi in wrk"
-
-# op = cr.correct_spell(text)
-# print op
