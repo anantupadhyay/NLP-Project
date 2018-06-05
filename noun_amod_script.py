748f0eab0c0e0e5b5d82f828acff175e6ff6516c
anantupadhyay <anantupadhyay17@gmail.com>
2018-06-05 16:29:59 +0530

    --

--- noun_amod_script.py
+++ noun_amod_script.py
@@ -1,30 +1,82 @@
+# from pycorenlp import StanfordCoreNLP
+# import json
+
+# def dependencyAnalysis(text):
+# 	try:
+# 		output = StanfordCoreNLP('http://13.127.253.52:9000/').annotate(text, properties={'annotators': "tokenize,ssplit,pos,depparse", 'pipelineLanguage': 'en', 'outputFormat':'json'})
+# 		wjson = json.dumps(output['sentences'])
+# 		data = json.loads(wjson)
+# 		noun = dict() # Maps nouns with their indexs
+# 		idx = set() # Stores index of all nouns which are related by nsubj
+# 		noun_list = list() # final list of output
+# 		check_list = ['NN', 'NNP', 'NNS']
+# 		for x in range(len(data[0]['tokens'])):
+# 			if(data[0]['tokens'][x]['pos'] in check_list):
+# 				noun[data[0]['tokens'][x]['index']] = data[0]['tokens'][x]['word']
+# 		for x in range(len(data[0]['enhancedPlusPlusDependencies'])):
+# 			tmp = data[0]['enhancedPlusPlusDependencies'][x]
+# 			if(tmp['dep']=='nsubj'):
+# 				x1 = tmp['dependent']
+# 				x2 = tmp['governor']
+# 				if ((data[0]['tokens'][x1-1]['pos'] in check_list) and (data[0]['tokens'][x2-1]['pos'] in check_list)):
+# 					idx.add(x2)
+# 		for x in range(len(data[0]['enhancedPlusPlusDependencies'])):
+# 			tmp = data[0]['enhancedPlusPlusDependencies'][x]
+# 			if((tmp['governor']) in idx and tmp['dep']=='amod'):
+# 				s1 = tmp['dependentGloss'] + "_" + tmp['governorGloss']
+# 				noun_list.append(s1)
+# 		print noun_list
+
+# 	except Exception as err:
+# 		print ("Oops! Error Occured"), err
+
+# if __name__=="__main__" :
+# 	try:
+# 		text = "The managing person is a good person"
+# 		text = "more staff should be at reception"
+# 		res = dependencyAnalysis(text)
+# 		if res != None:
+# 			print res
+
+# 	except Exception as err:
+# 		print ("Error Occured"), err
+
 from pycorenlp import StanfordCoreNLP
 import json
-
+from collections import defaultdict
 def dependencyAnalysis(text):
 	try:
 		output = StanfordCoreNLP('http://13.127.253.52:9000/').annotate(text, properties={'annotators': "tokenize,ssplit,pos,depparse", 'pipelineLanguage': 'en', 'outputFormat':'json'})
 		wjson = json.dumps(output['sentences'])
 		data = json.loads(wjson)
 		noun = dict() # Maps nouns with their indexs
-		idx = set() # Stores index of all nouns which are related by nsubj
+		idx = defaultdict(list) # Stores index of all nouns relations
 		noun_list = list() # final list of output
-		check_list = ['NN', 'NNP', 'NNS']
+		check_list = ['NN', 'NNP', 'NNS', 'PRP']
 		for x in range(len(data[0]['tokens'])):
 			if(data[0]['tokens'][x]['pos'] in check_list):
 				noun[data[0]['tokens'][x]['index']] = data[0]['tokens'][x]['word']
+		print noun
 		for x in range(len(data[0]['enhancedPlusPlusDependencies'])):
 			tmp = data[0]['enhancedPlusPlusDependencies'][x]
-			if(tmp['dep']=='nsubj'):
-				x1 = tmp['dependent']
-				x2 = tmp['governor']
-				if ((data[0]['tokens'][x1-1]['pos'] in check_list) and (data[0]['tokens'][x2-1]['pos'] in check_list)):
-					idx.add(x2)
+			if tmp['dep']=='nsubj':
+				idx[tmp['dependent']].append(tmp['governor'])
+
 		for x in range(len(data[0]['enhancedPlusPlusDependencies'])):
 			tmp = data[0]['enhancedPlusPlusDependencies'][x]
-			if((tmp['governor']) in idx and tmp['dep']=='amod'):
-				s1 = tmp['dependentGloss'] + "_" + tmp['governorGloss']
-				noun_list.append(s1)
+			if tmp['governor'] in noun.keys() and tmp['governor'] not in idx.keys() and tmp['dep']=='amod':
+				s = tmp['dependentGloss'] + "_" + tmp['governorGloss']
+				noun[tmp['governor']] = s
+			elif tmp['governor'] in noun.keys() and tmp['governor'] in idx.keys() and tmp['dep']!='det':
+				idx[tmp['governor']].append(tmp['dependent'])
+		sen = text.split()
+		for k,v in idx.items():
+			for val in v:
+				if val in noun.keys():
+					noun_list.append((noun[k],noun[val]))
+				else:
+					noun_list.append((noun[k],sen[val-1]))
+
 		print noun_list
 
 	except Exception as err:
@@ -33,7 +85,7 @@ def dependencyAnalysis(text):
 if __name__=="__main__" :
 	try:
 		text = "The managing person is a good person"
-		#text = "The managing director was humble person"
+		text = "more staff should be at reception"
 		res = dependencyAnalysis(text)
 		if res != None:
 			print res
--- spell check.py
+++ spell check.py
@@ -18,11 +18,19 @@
 # spell = translator.translate(txt, dest='en')
 # op = (spell.text).encode("utf-8")
 # print op
+from enchant.checker import SpellChecker
+chkr = SpellChecker("en_US")
+op = "this is sample staf"
+chkr.set_text(op)
+for err in chkr:
+    sug = err.suggest()[0]
+    err.replace(sug)
 
-import jamspell
-import jamspell
+c = chkr.get_text()#returns corrected text
+print c
 
-corrector = jamspell.TSpellCorrector()
-corrector.LoadLangModel('model_en.bin')
-
-print corrector.FixFragment('I am the begt spell cherken!')
+from googletrans import Translator
+translator = Translator()
+spell = translator.translate(op, dest='en')
+opt = (spell.text).encode("utf-8")
+print opt
