bb4fc086a486b8287951fb9ff0296280340e61fe
anantupadhyay <anantupadhyay@github.com>
2018-06-14 12:45:59 +0530

    NamedEntity

--- namedentity.py
+++ namedentity.py
@@ -2,12 +2,13 @@ from pycorenlp import StanfordCoreNLP
 
 def namedEntityRecognisition(text):
 	nlp = StanfordCoreNLP('http://13.127.253.52:9000/')
-	output = nlp.annotate(text, properties={'annotators': 'kbp','outputFormat':'json'})
-	for x in range(len(output['sentences'][0]['entitymentions'])):
-		tmp = output['sentences'][0]['entitymentions'][x]['text']
-		fin = '-'.join(tmp.split())
-		text = text.replace(tmp, fin)
-	return text
+	output = nlp.annotate(text, properties={'annotators': 'ssplit','outputFormat':'json'})
+	# for x in range(len(output['sentences'][0]['entitymentions'])):
+	# 	tmp = output['sentences'][0]['entitymentions'][x]['text']
+	# 	fin = '-'.join(tmp.split())
+	# 	text = text.replace(tmp, fin)
+	# return text
+	print output['sentences'][0]
 
 # def namedEntityRecognisition(text):
 # 	nlp = StanfordCoreNLP('http://13.127.253.52:9000/')
